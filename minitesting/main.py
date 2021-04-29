f57ef0f4b3bd832e491131c1a87b3a3e4126a4ff
şşş... [kuudere] <nicefriendly4@gmail.com>
2021-04-29 03:43:02 +0300

    python
    "versiyon:0.0.1"
    "history:29/04/2021"
    "encoder who:@ssskuudere"

---
+++ minitesting/main.py
@@ -0,0 +1,34 @@
+print("Hello world")#Dünyaya selam veriyor.
+print("29","04","2021",sep = "/") #Bu satır bu yazılımın doğum tarihi.
+print("I was born on this date") #Ben bu tarihte doğdum. Yazıyor.
+print("Encoder who \n@ssskuudere")#Kodlyıcının kim ve github hesap ismini yazıyor.
+print("{} + {} = {}".format(9057,2574,9057 + 2574 ))#Can sıkıntısı Matamtik sorusu :D
+
+a = 3 #Anahtar
+b = 2.14 #Anahtar
+c = "python" #Anahtar
+d = [1,2,3,4,5,"python"] #Anahtar
+e = (1,2,3,4,5,"python") #Anahtar
+f = {"ayıcık":139548} #Anahtar
+g = True #Anahtar
+
+print(type(a))#Anahtar Tanımlama.
+print(type(b))#Anahtar Tanımlama.
+print(type(c))#Anahtar Tanımlama.
+print(type(d))#Anahtar Tanımlama.
+print(type(f))#Anahtar Tanımlama.
+print(type(g))#Anahtar Tanımlama.
+
+print(f,e)#Anahtarın Cevabı.
+
+print(10 + 10)#Mini Matamatik. Toplama
+print(10 - 10)#Mini Matamatik. Çıkarma
+print(10 * 10)#Mini Matamatik. Çarpma
+print(10 / 10)#Mini Matamatik. Bölme
+print(10 // 10)#Mini Matamatik. Bölme ama kalansız
+print(10 % 10)#Mini Matamatik. bölme ama Kalanını verir
+
+"""
+script
+"""
+
