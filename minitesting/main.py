d9c9c9a0eb195fc778067f59685019faef2cc87b
şşş... [kuudere] <nicefriendly4@gmail.com>
2021-04-29 19:44:27 +0300

    encoder who @ssskuudere
    "versiyon: 0.0.2"
    "history:29/04/2021"
    "encoder discord:@şşş...[kuudere]#4465"

--- minitesting/main.py
+++ minitesting/main.py
@@ -1,4 +1,5 @@
-print("Hello world")#Dünyaya selam veriyor.
+print("Hello")#Selam veriyor.
+"""
 print("29","04","2021",sep = "/") #Bu satır bu yazılımın doğum tarihi.
 print("I was born on this date") #Ben bu tarihte doğdum. Yazıyor.
 print("Encoder who \n@ssskuudere")#Kodlyıcının kim ve github hesap ismini yazıyor.
@@ -28,7 +29,68 @@ print(10 / 10)#Mini Matamatik. Bölme
 print(10 // 10)#Mini Matamatik. Bölme ama kalansız
 print(10 % 10)#Mini Matamatik. bölme ama Kalanını verir
 
+h = 985
+t =574
+
+c = h + 58 * t
+
+print(c)
+
+
+
+
+"""
+"""
+b = "Hello_I'm_@ssskuudere"#Liste
+a = [1,2,5,9,3]#Liste
+print(a[0])#Liste
+print(b[10])#Liste
+print(len(b))#Liste
+print(a[len(a) - 5])#Liste
+print(b[0:2])#Liste
+"""
+"""
+takmaad = input("takma ad girin:")#İnput
+şifre = input("şifrenizi girin:")#İnput
+print("takam adınız" ,takmaad)#İnput
+print("şifireniz" , şifre)#İnput
+"""
+
+yas = int(input("yaşınızzı girin:"))#int İnput
+if yas < 18:
+    print(">")#Print
+print("<")#Print
+takmaad = input("takma ad girin:")#İnput
+sifre = input("şifrenizi girin:")#İnput
+
+#< değerden < true >false >
+#<= eşit se true deyilse ve değerde > false >=
+#== 2 değer eşitse true değilse false ==
+#!= eşitse false değilse true !=
+
+
+"""
+print("küçük bir test")
+m = int(input("a:"))#İnt input
+d = int(input("b:"))#İnt input
+print("toplam",m + d)#İnt input
+"""
+print("takam adınız" ,takmaad)#Print takma adı cevabı
+print("şifireniz" , sifre)#Print şifresi cevabı
+print("yaşınız", yas)#Print yaşı cevabı
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 script
 """
-
