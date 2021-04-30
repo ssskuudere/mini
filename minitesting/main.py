376731836aa78abfefde36bfd4bd2eed077943c3
şşş... [kuudere] <nicefriendly4@gmail.com>
2021-04-30 17:47:18 +0300

    encoder who:@ssskuudere
    "versiyon:0.0.3"
    "history30/04/2021"
    "
    test is over, other tests will be done
    "

--- minitesting/main.py
+++ minitesting/main.py
@@ -1,5 +1,5 @@
 print("Hello")#Selam veriyor.
-"""
+
 print("29","04","2021",sep = "/") #Bu satır bu yazılımın doğum tarihi.
 print("I was born on this date") #Ben bu tarihte doğdum. Yazıyor.
 print("Encoder who \n@ssskuudere")#Kodlyıcının kim ve github hesap ismini yazıyor.
@@ -39,8 +39,7 @@ print(c)
 
 
 
-"""
-"""
+
 b = "Hello_I'm_@ssskuudere"#Liste
 a = [1,2,5,9,3]#Liste
 print(a[0])#Liste
@@ -48,20 +47,20 @@ print(b[10])#Liste
 print(len(b))#Liste
 print(a[len(a) - 5])#Liste
 print(b[0:2])#Liste
-"""
-"""
+
 takmaad = input("takma ad girin:")#İnput
 şifre = input("şifrenizi girin:")#İnput
 print("takam adınız" ,takmaad)#İnput
 print("şifireniz" , şifre)#İnput
-"""
 
 yas = int(input("yaşınızzı girin:"))#int İnput
-if yas < 18:
-    print(">")#Print
-print("<")#Print
-takmaad = input("takma ad girin:")#İnput
-sifre = input("şifrenizi girin:")#İnput
+if yas < 13:
+    print("Lütfen daha sornradan tekrar deneyin.")#Print
+
+else:
+    print("Hoşgeldin.")
+    takmaad = input("takma ad girin:")#İnput
+    sifre = input("şifrenizi girin:")#İnput
 
 #< değerden < true >false >
 #<= eşit se true deyilse ve değerde > false >=
@@ -69,28 +68,44 @@ sifre = input("şifrenizi girin:")#İnput
 #!= eşitse false değilse true !=
 
 
-"""
+
 print("küçük bir test")
 m = int(input("a:"))#İnt input
 d = int(input("b:"))#İnt input
 print("toplam",m + d)#İnt input
-"""
+
+
 print("takam adınız" ,takmaad)#Print takma adı cevabı
 print("şifireniz" , sifre)#Print şifresi cevabı
 print("yaşınız", yas)#Print yaşı cevabı
 
+işlem = int(input("Bir işlem giriniz lütfen:"))
+if işlem == 12:
+    print("İşlem 12. yı girdiniz.")
+elif işlem  == 3:
+    print("İşlem 13. yı girdiniz.")
+elif işlem  == 2:
+    print("İşlem 3. yı girdiniz.")
+elif işlem  == 1:
+    print("İşlem 1. yı girdiniz.")
+else:
+    print("Hatalı işlem.Exit.")
 
 
 
+a = 512
+b = 852
+
+if a == 512 and b == 82:
+    print("Yes.")
+    print("Exit.")
+else:
+    print("No.")
+    print("Exit.")
+
+if (not (512 > 3)):
+    print("Yes.")
+else:
+    print("No.")
 
 
-
-
-
-
-
-
-
-"""
-script
-"""
