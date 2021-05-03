ff6b74bc68b39e45ca198dc62be752c0e1d01c7c
şşş... [kuudere] <nicefriendly4@gmail.com>
2021-05-03 16:24:34 +0300

    "discord:sss kawaii#4465"
    s

--- minitesting/main.py
+++ minitesting/main.py
@@ -1,4 +1,4 @@
-#   Tip         #   KÄ±saltma        #               Ã–rnek                   #
+#   Tip         #   Kýsaltma        #               Örnek                   #
 #----------------------------------------------------------------------------
 #   Integar     #   int and input   #   3, 10                               #
 #   Float       #   float           #   1.5, 7.0                            #
@@ -9,15 +9,24 @@
 #   Disctionary #   dict            #   {"github":"@ssskuudere, "age":12}   #
 #   Tuple       #   tup             #   (1, 2, true)                        #
 
+print("Hello world.")
+print("2/05/2021")
+
 def chat():
+
     a = int(input ("Your Age enter:"))
     if a >13:
-        print("hi")
         b = input("Your Nickname enter:")
         c = input("Your Password enter:")
         print("your age", a)
         print("Your Nickname", b)
         print("Your Password", c)
+        if d != 48:
+            print("Hi admin", a)
+        else:
+            print("Gg.")
+            print("Exited.")
+            print("Byee.")
     elif a ==13:
         print("hi")
         b = input("Your Nickname enter:")
@@ -25,6 +34,13 @@ def chat():
         print("your age", a)
         print("Your Nickname", b)
         print("Your Password", c)
+        a = input(":D:")
+        if d != 48:
+            print("Hi admin", a)
+        else:
+            print("Gg.")
+            print("Exited.")
+            print("Byee.")
     else:
         print("Exited.")
         print("Byee.")
